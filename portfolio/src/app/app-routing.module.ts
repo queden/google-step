73b23b16474b4426a01c2a416ceeb1242a93b853
Caden Felton <cadenmfelton@gmail.com>
2020-05-28 11:24:18 -0400

    Create list item template to be used in work and experience page

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -6,11 +6,10 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent },
+    { path: '', component: HomeComponent },
     { path: 'work', component: WorkComponent },
     { path: 'projects', component: ProjectsComponent },
     { path: 'contact', component: ContactComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
--- portfolio/src/app/work/work.component.ts
+++ portfolio/src/app/work/work.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListItem } from '../listItem'
 
 @Component({
   selector: 'app-work',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work.component.css']
 })
 export class WorkComponent implements OnInit {
-
+  // initializes empty items object to be initialized with work experience
+  items
   constructor() { }
 
   ngOnInit(): void {
+      // populates items list with work experience
+      this.items = [
+          new ListItem('Google', 'Working at Google', 'assets/img/goog.png'),
+          new ListItem('MLevel', 'Worked at MLevel', 'assets/img/mlevel.png')
+      ];
   }
 
 }
