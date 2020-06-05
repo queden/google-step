91fcae1de60b793517be1a10293f9e44863ff1a6
Caden <cadenmfelton@gmail.com>
2020-06-05 16:03:21 +0000

    Rebased on master

--- portfolio/src/main/angular/portfolio/src/app/work/work.component.ts
+++ portfolio/src/main/angular/portfolio/src/app/work/work.component.ts
@@ -15,7 +15,7 @@ export class WorkComponent implements OnInit {
       // populates items list with work experience
       this.items = [
           new ListItem('Google', 'May 2020 - Present', 'Edge TPU Team', 'assets/img/goog.png'),
-          new ListItem('MLevel', 'January 2019 - August 2019', 'Software Development Intern', 'assets/img/mlevel.png')\
+          new ListItem('MLevel', 'January 2019 - August 2019', 'Software Development Intern', 'assets/img/mlevel.png')
       ];
   }
 
