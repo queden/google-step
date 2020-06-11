47b8e0b623854181dfe17eaa6d2aabd2db2609fb
Caden <cadenmfelton@gmail.com>
2020-06-11 17:30:48 +0000

    Implement FindMeetingQuery to pass all tests

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,43 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    List<TimeRange> eventTimes = new ArrayList<TimeRange>();
+    // First find all events with attendees, add their time range to a list
+    for (Event event : events) {
+      if (!Collections.disjoint(event.getAttendees(), request.getAttendees())) {
+        eventTimes.add(event.getWhen());
+      }
+    }
+
+    // Sort time range list by start time
+    Collections.sort(eventTimes, TimeRange.ORDER_BY_START);
+
+    // Starting from beginning of day, look for time ranges between events with
+    // at least the amount of time needed
+    int lastTime = TimeRange.START_OF_DAY;
+    Collection<TimeRange> possibleTimes = new ArrayList<TimeRange>();
+    for (TimeRange time : eventTimes) {
+      if (time.start() - lastTime >= request.getDuration()) {
+        possibleTimes.add(TimeRange.fromStartEnd(lastTime, time.start(), false));
+      }
+
+      if (time.end() >= lastTime) {
+        lastTime = time.end();
+      }
+    }
+
+    if (lastTime < TimeRange.END_OF_DAY
+        && (TimeRange.END_OF_DAY - lastTime) >= request.getDuration()) {
+      possibleTimes.add(TimeRange.fromStartEnd(lastTime, TimeRange.END_OF_DAY, true));
+    }
+
+    return possibleTimes;
   }
 }
